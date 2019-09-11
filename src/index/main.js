6046dfefa527cb0ffb4a8139413e76a3ba9b7864
Andrew Byrne <abyrne@cartrawler.com>
2019-09-11 14:37:35 +0100

    remove dud functions

--- src/index/main.js
+++ src/index/main.js
@@ -5,19 +5,3 @@ require('./page.css')
 document.addEventListener('DOMContentLoaded', () => {
   console.log('DOMContentLoaded', 'page-index')
 })
-
-class Foo {
-  constructor () {
-    this.value = 'foobar'
-  }
-
-  static instance () {
-    return new Foo()
-  }
-
-  getValue () {
-    return this.value
-  }
-}
-
-console.log(Foo.instance().getValue())
